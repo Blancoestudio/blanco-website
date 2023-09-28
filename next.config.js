c92d2045c69f50b8d2d1ba5fda52d88e2b388d25
Derkysan <derkysan19@gmail.com>
2023-09-27 23:18:24 -0300

    fix: next config basePath error

--- next.config.js
+++ next.config.js
@@ -8,9 +8,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_SITE_URL: 'https://derkysan.github.io/blanco-website/',
-  },
+  // env: {
+  //   NEXT_PUBLIC_SITE_URL: 'https://derkysan.github.io/blanco-website/',
+  // },
 }
 
 module.exports = nextConfig
