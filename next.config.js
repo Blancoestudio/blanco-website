27ddcf97aba13cf80320fd9be9702577ae4b04e9
Derkysan <derkysan19@gmail.com>
2023-09-28 09:40:10 -0300

    fix: contact form

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: isProd ? 'export' : 'standalone',
-  basePath: isProd ? '/blanco-website' : '',
-  basePath: '',
+  // basePath: isProd ? '/blanco-website' : '',
+  basePath: '/blanco-website',
   images: {
     unoptimized: true,
   },
-  // env: {
-  //   NEXT_PUBLIC_SITE_URL: 'https://derkysan.github.io/blanco-website/',
-  // },
+  env: {
+    NEXT_PUBLIC_SITE_URL: 'https://derkysan.github.io/blanco-website/',
+  },
 }
 
 module.exports = nextConfig
