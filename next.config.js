ac6ad9ee47dcd1c7140bddd8e8e46cc2a36b9890
Derkysan <derkysan19@gmail.com>
2023-09-28 01:31:41 -0300

    fix: setting next config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: isProd ? 'export' : 'standalone',
-  // basePath: isProd ? '/blanco-website' : '',
+  basePath: isProd ? '/blanco-website' : '',
   images: {
     unoptimized: true,
   },
-  // env: {
-  //   NEXT_PUBLIC_SITE_URL: 'https://derkysan.github.io/blanco-website/',
-  // },
+  env: {
+    NEXT_PUBLIC_SITE_URL: 'https://derkysan.github.io/blanco-website/',
+  },
 }
 
 module.exports = nextConfig
