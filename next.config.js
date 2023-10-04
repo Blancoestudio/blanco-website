6d7a2dc2462a2f333c38384bc09c663ccec6c6b5
Derkysan <derkysan19@gmail.com>
2023-10-04 17:47:09 -0300

    fix next config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,15 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: isProd ? 'export' : 'standalone',
-  basePath: isProd ? '' : '',
+  basePath: isProd ? 'https://blanco-website-rg5jl.ondigitalocean.app/' : '',
   // output: 'export',
   distDir: '_static',
   images: {
     unoptimized: true,
   },
-  // env: {
-  //   NEXT_PUBLIC_SITE_URL: 'https://derkysan.github.io/blanco-website/',
-  // },
+  env: {
+    NEXT_PUBLIC_SITE_URL: 'https://blanco-website-rg5jl.ondigitalocean.app/',
+  },
 }
 
 module.exports = nextConfig
