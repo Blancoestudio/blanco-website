ff2e27044203990bbb4ecfda4f010d5889488996
Derkysan <derkysan19@gmail.com>
2023-10-24 21:08:49 -0300

    fix contact form

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: isProd ? 'export' : 'standalone',
-  // basePath: '/.',
-  basePath: '',
+  basePath: '/.',
+  // basePath: '',
   distDir: '_static',
   images: {
     unoptimized: true,
