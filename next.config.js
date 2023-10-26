5afb467778ee014d8e205128677c372a7c9bc8b0
Derkysan <derkysan19@gmail.com>
2023-10-26 00:10:07 -0300

    fixing deploy error

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   // output: isProd ? 'export' : 'standalone',
-  basePath: '/.',
+  // basePath: '/.',
   // basePath: '',
   distDir: '_static',
   images: {
